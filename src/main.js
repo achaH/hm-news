58411c87a88c2f20bf2bdd597e526efd42b3bfe8
achaH <hanchunyan980225@163.com>
2020-08-26 22:02:25 +0800

    配置登录和注册路由

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/base.less'
+import './styles/iconfont.css'
+import { Button } from 'vant'
+
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login">
+    这是登录组件
+    <i class="iconfont iconweixin"></i>
+    <i class="iconfont icondianzan"></i>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
